5023c0f306694d1a3858ef16e1622aff1bdfb1e2
Raghav <raghavbe@outlook.com>
2025-10-16 21:01:10 +0530

    Product Service Initial Commit with Product and Category models

---
+++ src/main/java/com/capstone/productservice/models/Product.java
@@ -0,0 +1,15 @@
+package com.capstone.productservice.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private Category category;
+}
